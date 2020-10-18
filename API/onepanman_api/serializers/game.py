64d6e6212e28642a0e1ee4bee73e63550b99f4a5
CMS0503 <tyms0503@gmail.com>
2020-10-19 01:25:09 +0900

    fix error

--- API/onepanman_api/serializers/game.py
+++ API/onepanman_api/serializers/game.py
@@ -4,14 +4,14 @@ from .. import models
 
 
 class GameSerializer(serializers.ModelSerializer):
-    challenger = serializers.ReadOnlyField(source='challenger_code.author')
-    opposite = serializers.ReadOnlyField(source='opposite_code.author')
+    challenger = serializers.ReadOnlyField(source='challenger_code.author.id')
+    opposite = serializers.ReadOnlyField(source='opposite_code.author.id')
     challenger_name = serializers.ReadOnlyField(source='challenger_code.author.username')
     opposite_name = serializers.ReadOnlyField(source='opposite_code.author.username')
-    problem = serializers.ReadOnlyField(source='challenger_code.author.problem')
+    problem = serializers.ReadOnlyField(source='challenger_code.problem.id')
 
     class Meta:
         model = models.Game
-        # fields = ['id', 'problem', 'placement_record', 'challenger', 'opposite', 'record', 'winner', 'date',
-        #           'challenger_code', 'opposite_code', 'result', 'error_msg', 'challenger_name', 'opposite_name', 'type']
+        # fields = ['id', 'placement_record', 'record', 'winner', 'date', 'problem',
+        #           'challenger_code', 'opposite_code', 'result', 'error_msg', 'type']
         fields = "__all__"
