b0698103005778d01280bb04d7cadb1bf87cbee3
CMS0503 <tyms0503@gmail.com>
2020-10-20 02:06:51 +0900

    Delete unused admin

--- API/onepanman_api/admin/__init__.py
+++ API/onepanman_api/admin/__init__.py
@@ -5,6 +5,4 @@ from .groupInfo import GroupInfoAdmin
 from .language import LanguageAdmin
 from .userInfo import UserInfoAdmin
 
-__all__ = ['ProblemAdmin', 'ArticleAdmin', 'CodeAdmin', 'CommentAdmin', 'FriendAdmin', 'GameAdmin',
-           'GroupInfoAdmin', 'LanguageAdmin', 'NoticeAdmin', 'TestcaseAdmin', 'UserInfoAdmin',
-           'UserInformationInProblemAdmin', 'RuleAdmin']
+__all__ = ['ProblemAdmin', 'CodeAdmin', 'GameAdmin', 'GroupInfoAdmin', 'LanguageAdmin', 'UserInfoAdmin',]
