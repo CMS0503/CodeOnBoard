fa6d759b7e3ce1a12e7cf4be748b76ce16da9931
CMS0503 <tyms0503@gmail.com>
2020-10-20 23:46:08 +0900

    Fix typo

--- API/onepanman_api/admin/__init__.py
+++ API/onepanman_api/admin/__init__.py
@@ -4,6 +4,6 @@ from .game import GameAdmin
 from .groupInfo import GroupInfoAdmin
 from .language import LanguageAdmin
 from .userInfo import UserInfoAdmin
-from .rule import Rule
+from .rule import RuleAdmin
 
-__all__ = ['ProblemAdmin', 'CodeAdmin', 'GameAdmin', 'GroupInfoAdmin', 'LanguageAdmin', 'UserInfoAdmin', 'Rule']
+__all__ = ['ProblemAdmin', 'CodeAdmin', 'GameAdmin', 'GroupInfoAdmin', 'LanguageAdmin', 'UserInfoAdmin', 'RuleAdmin']
