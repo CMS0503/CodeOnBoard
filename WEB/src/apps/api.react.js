a7feaa0617a391bf031443298ea68c382e0122da
CMS0503 <tyms0503@gmail.com>
2020-10-24 01:52:00 +0900

    Add api address

---
+++ WEB/src/apps/api.react.js
@@ -0,0 +1,64 @@
+import axios from 'axios'
+
+const corsUrl = 'https://cors-anywhere.herokuapp.com'
+// const baseUrl = 'http://203.246.112.32:8000/api/v1'
+const baseUrl = 'http://127.0.0.1:8000/api/v1'
+const authJWT = 'jwt ' + window.localStorage.getItem('jwt')
+
+export function getProblem(problemId){
+    const header = {
+        'Authorization': authJWT
+
+    }
+    return axios.get(`/api/v1/problem/${problemId}`,{})
+    // ${corsUrl}/
+}
+
+export function getUserInfo(userId){
+    return axios.get(`/api/v1/userfullInfo/${userId}`, {})
+    // ${corsUrl}/
+}
+
+export function getCodeList(userId, problemId){
+    return axios.get(`/api/v1/code/?author=${userId}&problem=${problemId}&available_game=true`)
+}
+
+export function matching(data){
+    return axios.post(`/api/v1/match/`, data, {})
+    // ${corsUrl}/
+}
+
+export function getGame(gameId){
+    return axios.get(`/api/v1/game/${gameId}/`, {})
+}
+
+export function getProblems(){
+    return axios.get(`/api/v1/problem/`)
+}
+
+export function postCode(data){
+    return axios.post(`/api/v1/code/`, data, {})
+    // ${corsUrl}/
+}
+
+export function getCode(codeId){
+    const header = {
+        'Authorization': authJWT
+    }
+    return axios.get(`/api/v1/code/${codeId}`, { headers: header})
+    // ${corsUrl}/
+}
+
+export function getGames(){
+    const header = {
+        'Authorization': authJWT
+    }
+    return axios.get(`/api/v1/game/my`, {headers: header})
+}
+
+export function selfBattle(bodyData){
+    var header = {
+        'Authorization' : authJWT
+    }
+    return axios.post(`/api/v1/selfBattle/`, bodyData, { headers: header})
+}
