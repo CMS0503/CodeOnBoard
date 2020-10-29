6b08d97e6c9fec22da26e2d30896109e3fa2131a
CMS0503 <tyms0503@gmail.com>
2020-10-30 00:46:41 +0900

    Add axios interceptors

--- WEB/src/apps/api/api.react.js
+++ WEB/src/apps/api/api.react.js
@@ -1,14 +1,10 @@
-import axios from 'axios'
 import axiosAPI from "./axiosApi";
 
-const corsUrl = 'https://cors-anywhere.herokuapp.com'
-// const baseUrl = 'http://203.246.112.32:8000/api/v1'
-const baseUrl = 'http://127.0.0.1:8000/api/v1'
 const authJWT = 'jwt ' + window.localStorage.getItem('access_token')
 
 export function register(username, email, password1, password2){
     console.log("regi", username, email, password1)
-    return axios.post("rest-auth/registration/", {
+    return axiosAPI.post("rest-auth/registration/", {
                         username:username,
                         password1:password1,
                         password2:password2,
@@ -16,47 +12,50 @@ export function register(username, email, password1, password2){
                       })
 }
 export function getProblem(problemId){
-    const header = {
-        'Authorization': authJWT
-
-    }
-    return axios.get(`/api/v1/problem/${problemId}`,{})
-    // ${corsUrl}/
+    return axiosAPI.get(`/api/v1/problem/${problemId}/`,{})
 }
 
 export function getUserInfo(userId){
-    return axios.get(`/api/v1/userfullInfo/${userId}`, {})
-    // ${corsUrl}/
+    return axiosAPI.get(`/api/v1/userfullInfo/${userId}/`, {})
 }
 
 export function getCodeList(userId, problemId){
-    return axios.get(`/api/v1/code/?author=${userId}&problem=${problemId}&available_game=true`)
+    const params = {
+        problem:problemId
+    }
+
+    const config = {
+        params:params,
+    }
+    return axiosAPI.get(`/api/v1/code/my`, config)
 }
 
 export function matching(data){
-    return axios.post(`/api/v1/match/`, data, {})
+    return axiosAPI.post(`/api/v1/match/`, data, {})
     // ${corsUrl}/
 }
 
 export function getGame(gameId){
-    return axios.get(`/api/v1/game/${gameId}/`, {})
+    return axiosAPI.get(`/api/v1/game/${gameId}/`, {})
 }
 
 export function getProblems(){
-    return axios.get(`/api/v1/problem/`)
+    console.log("getProblems")
+    return axiosAPI.get(`/api/v1/problem/`)
 }
 
 export function postCode(data){
-    return axios.post(`/api/v1/code/`, data, {})
+    return axiosAPI.post(`/api/v1/code/`, data, {})
     // ${corsUrl}/
 }
 
 export function getCode(codeId){
-    const header = {
-        'Authorization': authJWT
-    }
-    return axios.get(`/api/v1/code/${codeId}`, { headers: header})
+    return axiosAPI.get(`/api/v1/code/${codeId}/`, {})
     // ${corsUrl}/
+}
+
+export function deleteCode(codeId){
+    return axiosAPI.delete(`api/v1/code/${codeId}/`, {})
 }
 
 export function getGames(problemId){
@@ -64,19 +63,13 @@ export function getGames(problemId){
     const params = {
         problem:problemId
     }
-    const header = {
-        'Authorization': authJWT,
-    }
+
     const config = {
         params:params,
-        headers:header
     }
-    return axios.get(`/api/v1/game/my`,  config)
+    return axiosAPI.get(`/api/v1/game/my`,  config)
 }
 
 export function selfBattle(bodyData){
-    var header = {
-        'Authorization' : authJWT
-    }
-    return axios.post(`/api/v1/selfBattle/`, bodyData, { headers: header})
+    return axiosAPI.post(`/api/v1/selfBattle`, bodyData, {})
 }
--- WEB/src/apps/api/axiosApi.js
+++ WEB/src/apps/api/axiosApi.js
@@ -1,35 +1,39 @@
 import axios from "axios";
 
-const baseURL = "http://203.246.112.32:8000" //"https://cors-anywhere.herokuapp.com/http://203.246.112.32:8000";
-const accessToken = localStorage.getItem("access_token");
+const baseURL = "http://127.0.0.1:8000" //"http://203.246.112.32:8000"
+const accessToken = localStorage.getItem("userInfo")
+    ?"jwt " + JSON.parse(localStorage.getItem("userInfo")).accessToken
+    :""
 
 const axiosAPI = axios.create({
   baseURL: baseURL,
   timeout: 5000,
-  // headers: {
-  //   Authorization: accessToken ? "JWT " + accessToken : null,
-  //   "Content-Type": "application/json",
-  //   accept: "application/json",
-  // },
+  headers: {
+    Authorization: accessToken,
+    "Content-Type": "application/json",
+    accept: "application/json",
+  },
 });
 
 axiosAPI.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    return response
+  },
   async (error) => {
     const originalRequest = error.config;
-
     // Prevent infinite loops
     if (
-      error.response.status === 401 &&
+      error.status === 401 &&
       originalRequest.url === baseURL + "token/refresh/"
     ) {
+
       window.location.href = "/login/";
       return Promise.reject(error);
     }
 
     if (
-      error.response.status === 401 &&
-      error.response.statusText === "Unauthorized"
+      error.status === 401 &&
+      error.statusText === "Unauthorized"
     ) {
       const refresh = localStorage.getItem("refresh_token");
 
@@ -78,3 +82,4 @@ export function setNewHeaders(response) {
 }
 
 export default axiosAPI;
+
