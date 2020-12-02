b217d5e006461247a33285e9ba26f45f1db9925a
CMS0503 <tyms0503@gmail.com>
2020-12-02 16:08:50 +0900

    Add check rule function

--- CORE/rule/rules.py
+++ CORE/rule/rules.py
@@ -9,3 +9,41 @@ class Rules(PlacementRule, ActionRule, EndingRule):
         ActionRule.__init__(self)
         EndingRule.__init__(self)
 
+    def check_rule(self, game_data, placement_data):
+        """
+        check user's placement conforms to the rule
+        :param placement_data:
+        :type placement_data:
+        :param game_data:
+        :type game_data:
+        :return error msg, new board, is ending, winner:
+        :rtype string | None:
+        """
+
+        # Start check placement rule
+        print('Check placement rule...', end='')
+        try:
+            check_placement, new_board = self.check_placement_rule(game_data, placement_data)
+        except Exception as e:
+            error_msg = f'Error in check placement rule : {e}'
+            return error_msg, None, True, 0
+        print(check_placement)
+
+        # Check Action Rule
+        print('Check action rule...', end='')
+        try:
+            apply_action, new_board = self.apply_action_rule(game_data, placement_data)
+        except Exception as e:
+            error_msg = f'Error in check action rule : {e}'
+            return error_msg, None, True, 0
+        print(apply_action)
+
+        # Check Ending Rule
+        print('Check ending rule...', end='')
+        try:
+            is_ending, winner = self.check_ending(game_data, placement_data)
+        except Exception as e:
+            error_msg = f'Error check ending rule : {e}'
+            return error_msg, None, True, 0
+
+        return None, new_board, is_ending, winner
