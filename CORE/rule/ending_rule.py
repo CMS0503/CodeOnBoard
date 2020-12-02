d35a176a9975b7bd6bf03e9905ddda0581a1a58d
CMS0503 <tyms0503@gmail.com>
2020-12-02 16:09:25 +0900

    Delete unused code

--- CORE/rule/ending_rule.py
+++ CORE/rule/ending_rule.py
@@ -53,48 +53,6 @@ class EndingRule:
         return self.is_ending, 0
 
 
-
-    # 엔딩 조건
-    def one_line(self, game_data, board, placement):  # TODO
-        print('######')
-
-        direction = np.array([[1, 0], [-1, 0], [1, 1], [-1, -1], [0, 1], [0, -1], [-1, 1], [1, -1]])
-        value = board[placement[0]][placement[1]]
-
-        direction_count = [1, 1, 1, 1]
-
-        for i in range(4):
-            x = placement[0]  # - 1
-            y = placement[1]  # - 1
-            new_value = value
-            while (0 < x < game_data.board_size) and (0 < y < game_data.board_size) and (new_value == value):
-                x += direction[i*2][1]
-                y += direction[i*2][0]
-                if x == game_data.board_size or y == game_data.board_size:
-                    break
-                new_value = board[x][y]
-                direction_count[i] += 1
-                if direction_count[i] == 5:
-                    self.ending_message = True
-                    print(direction_count)
-                    return self.ending_message, value
-
-            x = placement[0]  # - 1
-            y = placement[1]  # - 1
-            new_value = value
-            while (0 < x < game_data.board_size) and (0 < y < game_data.board_size) and (new_value == value):
-                x += direction[(i*2)+1][1]
-                y += direction[(i*2)+1][0]
-                if x == game_data.board_size or y == game_data.board_size:
-                    break
-                new_value = board[x][y]
-                direction_count[i] += 1
-                if direction_count[i] == 5:
-                    self.ending_message = True
-                    print('2', direction_count)
-                    return self.ending_message, value
-        return False, 0
-
     # 보드판을 가득 채웠을 경우
     def full_board(self):
         if np.any(self.board == 0) is False:
